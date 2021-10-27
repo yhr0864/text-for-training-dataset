52ea0a8589e1e5d53b4068e4a5c700a30feff657
yhr0864 <90278898+yhr0864@users.noreply.github.com>
2021-10-27 20:18:22 +0200

    Update text-generating.py

--- text-generating.py
+++ text-generating.py
@@ -2,35 +2,38 @@ import os
 import glob
 from tqdm import tqdm
 
+def textGenerate():
+    root_path1 = r'C:\Users\Myth\Desktop\data\input\train'
+    root_path2 = r'C:\Users\Myth\Desktop\data\output\train'
 
-root_path1 = r'C:\Users\Myth\Desktop\data\input\train'
-root_path2 = r'C:\Users\Myth\Desktop\data\output\train'
+    save_dir = os.path.normpath(root_path1)
+    save_dir2 = os.path.normpath(root_path2)
 
-save_dir = os.path.normpath(root_path1)
-save_dir2 = os.path.normpath(root_path2)
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+        print('create folder')
 
-if not os.path.exists(save_dir):
-    os.mkdir(save_dir)
-    print('create folder')
-    
-with open(r'C:\Users\Myth\Desktop\data\input\train.txt', 'w') as f:
-    path1 = root_path1 + '\*png'
-    path2 = root_path2 + '\*png'
-    
-    pictures_list1 = glob.glob(path1)
-    pictures_list2 = glob.glob(path2)
+    with open(r'C:\Users\Myth\Desktop\data\input\train.txt', 'w') as f:
+        path1 = root_path1 + '\*png'
+        path2 = root_path2 + '\*png'
 
-#     for root, dirs, files in os.walk(root_path1):
-#         for file in files:
-#             l1.append(str(file))
-                        
-    with tqdm(total=len(pictures_list1)) as bar:
-        idx = 0
-        for pic in pictures_list1:
-            
-            name = os.path.basename(os.path.normpath(pic))
-            label = os.path.basename(os.path.normpath(pictures_list2[idx]))
+        pictures_list1 = glob.glob(path1)
+        pictures_list2 = glob.glob(path2)
 
-            f.write(os.path.join(save_dir, name) + ' ' + os.path.join(save_dir2, label) + '\n')
-            idx = idx + 1
-            bar.update(1)
+    #     for root, dirs, files in os.walk(root_path1):
+    #         for file in files:
+    #             l1.append(str(file))
+
+        with tqdm(total=len(pictures_list1)) as bar:
+            idx = 0
+            for pic in pictures_list1:
+
+                name = os.path.basename(os.path.normpath(pic))
+                label = os.path.basename(os.path.normpath(pictures_list2[idx]))
+
+                f.write(os.path.join(save_dir, name) + ' ' + os.path.join(save_dir2, label) + '\n')
+                idx = idx + 1
+                bar.update(1)
+                
+if __name__=='__main__':
+    textGenerate()
